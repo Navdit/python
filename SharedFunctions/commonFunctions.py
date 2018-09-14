7e780882e9377012ffb9c017aad0ad7a81c4bb18
Navdit <42660836+Navdit@users.noreply.github.com>
2018-09-14 10:44:59 +1000

    Added write_to_excel function

--- SharedFunctions/commonFunctions.py
+++ SharedFunctions/commonFunctions.py
@@ -1,4 +1,6 @@
 import os
+from openpyxl import load_workbook
+import xlsxwriter
 
 ##################################################################################################################
 # Function Name: searchFile
@@ -90,4 +92,46 @@ def createCurrDateFolderName():
     strCurrFolder = year+month+day
     return strCurrFolder
         
+##################################################################################################################
+
+
+##################################################################################################################
+# Function Name: write_to_excel
+# Description  : Creates/Modifies the given excel
+# @param       : String: sheet_name
+# @param       : Dataframe: output_excel_df - dataframe, which will be written to excel
+# @param       : String: output_excel_loc - Location of the excel file
+# @param       : Boolean :append - True, to overwrite the existing excel file
+# @return      : Null
+##################################################################################################################
+def write_to_excel(sheet_name, output_excel_df, output_excel_loc, append=True):
+    # Check if Excel File exists
+    if not os.path.isfile(output_excel_loc):
+        workbook = xlsxwriter.Workbook(output_excel_loc)
+        workbook.close()
+
+    # Add data to current excel
+    if append:
+        workbook = load_workbook(output_excel_loc)
+        # Check if Sheet Exists
+        if sheet_name in workbook.sheetnames:
+            workbook.remove(workbook[sheet_name])
+
+        # Remove "Sheet1" from Excel
+        if "Sheet1" in workbook.sheetnames:
+            workbook.remove(workbook["Sheet1"])
+
+        # Now write the workbook to excel
+        writer = pd.ExcelWriter(output_excel_loc, engine='openpyxl')
+        writer.book = workbook
+
+    # Overwrite current excel
+    else:
+        writer = pd.ExcelWriter(output_excel_loc)
+
+    # Write to Excel
+    output_excel_df.to_excel(writer, sheet_name, index=False)
+    writer.save()
+    writer.close()
+
 ##################################################################################################################
