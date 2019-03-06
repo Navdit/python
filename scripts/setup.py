d3c910e5e88c52578399c81cf954d23e49919e8c
Navdit <42660836+Navdit@users.noreply.github.com>
2019-03-07 10:21:38 +1100

    Update setup.py

--- scripts/setup.py
+++ scripts/setup.py
@@ -1,11 +1,20 @@
 import time
 import importlib
 from pip._internal import main as pipmain
+import data.global_variables as global_var
 
 
 # FUNCTION - Installs the given Module in Python
 def install(package):
     pipmain(['install', package])
+
+
+# Reads Requirements from Requirements.txt
+def read_requirements():
+    with open(global_var.requirements_path) as f:
+        requirements = f.read().splitlines()
+        # print(requirements)
+    return requirements
 
 
 # Start
@@ -14,9 +23,9 @@ if __name__ == '__main__':
     # Start Time of Code
     start_time = time.time()
 
-    # Declare Dependencies
-    dependencies = ["pandas",
-                    "bokeh"]
+    read_requirements()
+
+    dependencies = read_requirements()
 
     # Check for dependencies
     try:
@@ -32,3 +41,4 @@ if __name__ == '__main__':
 
     # Print Time taken to execute script
     print("CUSTOM INFO : --- Script Execution Time: %s seconds ---" % (time.time() - start_time))
+
